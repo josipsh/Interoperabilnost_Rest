7a615d9d9855f3113e0c612d89caa43e4ea4a6b0
Josip <josip.sadek1998@gmail.com>
2022-06-07 23:35:29 +0200

    Enabled xml

--- IisRest.API/Program.cs
+++ IisRest.API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.ConfigureAuth(builder.Configuration);
 builder.Services.ConfigureAutoMapper(builder.Configuration);
 builder.Services.ConfigureDependecyInjection(builder.Configuration);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
